2b9bbe8887e175487473e7b0db41bf3cca6bfadf
Neil Weidinger <neilweidingern@gmail.com>
2018-03-19 17:37:19 -0500

    fixed indenting with sublime text and how mac saves files

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,11 +1,11 @@
 public class HelloNumbers {
-	public static void main(String[] args) {
-		int count = 0;
+    public static void main(String[] args) {
+       int count = 0;
 
-		for (int i = 0; i < 10; i++) {
-			System.out.print((count += i) + " ");
-		}
+        for (int i = 0; i < 10; i++) {
+            System.out.print((count += i) + " ");
+        }
 
-		System.out.println("");
-	}
+        System.out.println("");
+    }
 }
