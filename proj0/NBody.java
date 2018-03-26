db4340d08173afbc7c900b3cea05674cfb1d7a64
Neil Weidinger <neilweidingern@gmail.com>
2018-03-26 12:44:34 -0500

    forgot to commit Planet.java (finished) and NBody.java (unfinished)

---
+++ proj0/NBody.java
@@ -0,0 +1,33 @@
+public class NBody {
+    public static void main(String[] args) {
+        double T = Double.parseDouble(args[0]);
+        double dT = Double.parseDouble(args[1]);
+        String filename = args[2];
+
+        double radius = NBody.readRadius(filename);
+        Planet[] planets = NBody.readPlanets(filename);
+    }
+
+    public static double readRadius(String name) {
+        In in = new In(name);
+        in.readInt();
+        return in.readDouble();
+    }
+
+    public static Planet[] readPlanets(String name) {
+        In in = new In(name);
+        int numPlanets = in.readInt();
+        in.readDouble();
+
+        Planet[] planets = new Planet[numPlanets];
+        int i = 0;
+
+        while (i != numPlanets) {
+            planets[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(),
+                                    in.readDouble(), in.readDouble(), in.readString());
+            i++;
+        }
+
+        return planets;
+    }
+}
---
+++ proj0/Planet.java
@@ -0,0 +1,74 @@
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+    public static final double gConst = 6.67 / 1e11;
+
+    public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Planet(Planet p) {
+        this(p.xxPos, p.yyPos, p.xxVel, p.yyVel, p.mass, p.imgFileName);
+    }
+
+    public double calcDistance(Planet p) {
+        double xxDist = p.xxPos - this.xxPos;
+        double yyDist = p.yyPos - this.yyPos;
+
+        return Math.sqrt((xxDist * xxDist) + (yyDist * yyDist));
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        double r = this.calcDistance(p);
+
+        return (1/(r * r)) * (gConst * this.mass * p.mass);
+    }
+
+    public double calcForceExertedByX(Planet p) {
+        return this.calcForceExertedBy(p) * ((p.xxPos - this.xxPos) / this.calcDistance(p));
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        return this.calcForceExertedBy(p) * ((p.yyPos - this.yyPos) / this.calcDistance(p));
+    }
+
+    public double calcNetForceExertedByX(Planet[] planets) {
+        double total = 0.0;
+
+        for (Planet p : planets) {
+            if (!this.equals(p)) {
+                total += this.calcForceExertedByX(p);
+            }
+        }
+
+        return total;
+    }
+
+    public double calcNetForceExertedByY(Planet[] planets) {
+        double total = 0.0;
+
+        for (Planet p : planets) {
+            if (!this.equals(p)) {
+                total += this.calcForceExertedByY(p);
+            }
+        }
+
+        return total;
+    }
+
+    public void update(double dt, double fX, double fY) {
+        this.xxVel = this.xxVel + (dt * (fX / this.mass));
+        this.yyVel = this.yyVel + (dt * (fY / this.mass));
+        this.xxPos = this.xxPos + (dt * this.xxVel);
+        this.yyPos = this.yyPos + (dt * this.yyVel);
+    }
+}
