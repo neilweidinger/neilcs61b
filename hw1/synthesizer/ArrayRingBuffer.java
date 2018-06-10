7c61c03a8fd4d5b1a155c8cc91a84db220abef9f
Neil Weidinger <neilweidingern@gmail.com>
2018-06-10 11:18:45 +0900

    finished hw1

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,16 +1,31 @@
-// TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+package synthesizer;
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
-    private int first;            // index for the next dequeue or peek
+    private int first;
     /* Index for the next enqueue. */
     private int last;
     /* Array for storing the buffer data. */
     private T[] rb;
+
+    public class arbIterator implements Iterator<T>{
+
+        public boolean hasNext() {
+            return ArrayRingBuffer.this.first != ArrayRingBuffer.this.last;
+        }
+
+        public T next() {
+            return ArrayRingBuffer.this.dequeue();
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new arbIterator();
+    }
 
     /**
      * Create a new ArrayRingBuffer with the given capacity.
@@ -21,6 +36,11 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        this.rb = (T[]) new Object[capacity];
+        this.first = 0;
+        this.last = 0;
+        this.fillCount = 0;
+        this.capacity = capacity;
     }
 
     /**
@@ -30,6 +50,13 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (this.isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+
+        this.rb[last] = x;
+        this.fillCount += 1;
+        this.last = (this.last == this.capacity - 1) ? 0 : this.last + 1;
     }
 
     /**
@@ -39,6 +66,15 @@ public class ArrayRingBuffer<T>  {
      */
     public T dequeue() {
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        if (this.isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+
+        T temp = this.rb[first];
+        this.fillCount -= 1;
+        this.first = (this.first == this.capacity -1) ? 0 : this.first + 1;
+
+        return temp;
     }
 
     /**
@@ -46,6 +82,7 @@ public class ArrayRingBuffer<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        return this.rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -8,8 +8,69 @@ import static org.junit.Assert.*;
 
 public class TestArrayRingBuffer {
     @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+    public void testDequeue() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(5);
+        arb.enqueue("first");
+        arb.enqueue("second");
+
+        String returnVal = arb.dequeue();
+
+        assertEquals("first", returnVal);
+        assertEquals(1, arb.fillCount());
+        assertEquals(5, arb.capacity());
+    }
+
+    @Test
+    public void testEnqueue() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(5);
+        arb.enqueue("first");
+        arb.enqueue("second");
+
+        assertEquals("first", arb.peek());
+        assertEquals(2, arb.fillCount());
+        assertEquals(5, arb.capacity());
+    }
+
+    @Test
+    public void testFull() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(5);
+        arb.enqueue("first");
+        arb.enqueue("second");
+        arb.enqueue("third");
+        arb.enqueue("fourth");
+        arb.enqueue("fifth");
+
+        assertTrue(arb.isFull());
+        assertFalse(arb.isEmpty());
+    }
+
+    @Test
+    public void testEmpty() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(5);
+        assertTrue(arb.isEmpty());
+        assertFalse(arb.isFull());
+    }
+
+    @Test
+    public void testIteration() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(5);
+        String [] words = {
+                "first",
+                "second",
+                "third",
+                "fourth",
+                "fifth"
+        };
+
+        for (String s : words) {
+            arb.enqueue(s);
+        }
+
+        int index = 0;
+        for (String s : arb) {
+            assertEquals(words[index], s);
+            index++;
+        }
     }
 
     /** Calls tests for ArrayRingBuffer. */
